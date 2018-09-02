580e4d6def95a2755e1c40a57d83296af24849d9
Caleb Winston <calebhwin@gmail.com>
2018-09-01 23:07:09 -0700

    updated to es6

--- populate.js
+++ populate.js
@@ -1,18 +1,18 @@
 Element.prototype.populate = function(array, item) {
-    for (var i = 0; i < array.length; i++) {
-        var li;
+    for (let i = 0; i < array.length; i++) {
+        let li;
 	    
 	// add new element to store this data in if neccessary
-        var newElementNeeded = this.children.length <= i;
+        let newElementNeeded = this.children.length <= i;
         if (newElementNeeded) {
             li = document.createElement("li");
         } else {
             li = this.children[i];
         }
 	    
-        for (var j = 0; j < item.length; j++) {
-            var newComponentNeeded = li.children.length <= j;
-            var liComponent;
+        for (let j = 0; j < item.length; j++) {
+            let newComponentNeeded = li.children.length <= j;
+            let liComponent;
             if (newComponentNeeded) {
                 liComponent = document.createElement(item[j].tag);
             } else {
@@ -24,13 +24,14 @@ Element.prototype.populate = function(array, item) {
                     liComponent.removeAttribute(liComponent.attributes[0].name);
                 }
             }
-            for (var k = 0; k < item[j].properties.length; k++) {
+            for (let k = 0; k < item[j].properties.length; k++) {
                 liComponent.setAttribute(item[j].properties[k].attr, item[j].properties[k].val);
             }
             if (newComponentNeeded) {
                 li.appendChild(liComponent);
             }
         }
+	    
         if (newElementNeeded) {
             this.appendChild(li);
         }
